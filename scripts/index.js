a011798d2af9b6a9571c3e5524479febdc19ad36
Sofia Puolakainen <sonja.puolaka@yandex.ru>
2021-01-24 20:17:49 +0300

    feature: add new card

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,3 @@
-// попап для редактирования профиля
-let editButton = document.querySelector('.button_type_edit-profile')
-let popupEditProfile = document.querySelector('.popup_type_edit')
-let closeButton = popupEditProfile.querySelector('.button_type_close')
-
-let togglePopupEditProfile = () => {
-    popupEditProfile.classList.toggle('popup_opened')
-}
-
-editButton.addEventListener('click', togglePopupEditProfile)
-closeButton.addEventListener('click', togglePopupEditProfile)
-
-popupEditProfile.addEventListener('click', (event) => {
-    if (event.target === event.currentTarget) {
-        togglePopupEditProfile()
-    }
-})
-
-let form = popupEditProfile.querySelector('.popup__container')
-let inputs = popupEditProfile.querySelectorAll('input')
-let profileName = document.querySelector('.profile__name')
-let description = document.querySelector('.profile__description')
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    profileName.textContent = inputs[0].value
-    description.textContent = inputs[1].value
-    togglePopupEditProfile()
-})
-
-// попап для добавления карточек
-
-let addButton = document.querySelector('.button_type_add-card');
-let popupAddCard = document.querySelector('.popup_type_add')
-let closeButton1 = popupAddCard.querySelector('.button_type_close')
-
-let togglePopupAddCard = () => {
-  popupAddCard.classList.toggle('popup_opened')
-}
-
-addButton.addEventListener('click', togglePopupAddCard)
-closeButton1.addEventListener('click', togglePopupAddCard)
-
-popupAddCard.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget) {
-      togglePopupAddCard()
-  }
-})
-
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -75,10 +25,60 @@ const initialCards = [
   }
 ];
 
-const itemTemplate = document.querySelector(".item_template").content;
-const list = document.querySelector(".elements__list");
+
+let editButton = document.querySelector('.button_type_edit-profile');
+let popupEditProfile = document.querySelector('.popup_type_edit');
+let closeButton = popupEditProfile.querySelector('.button_type_close');
+let formEditProfile = popupEditProfile.querySelector('.popup__container');
+let inputsProfile = popupEditProfile.querySelectorAll('input');
+let profileName = document.querySelector('.profile__name');
+let description = document.querySelector('.profile__description');
+let addButton = document.querySelector('.button_type_add-card');
+let popupAddCard = document.querySelector('.popup_type_add');
+let inputs = popupAddCard.querySelectorAll('input');
+let closeButton1 = popupAddCard.querySelector('.button_type_close');
+let itemTemplate = document.querySelector('.item_template').content;
+let list = document.querySelector('.elements__list');
+let formAddCard = popupAddCard.querySelector('.popup__container');
 
 
+
+// попап для редактирования профиля
+let togglePopupEditProfile = () => {
+    popupEditProfile.classList.toggle('popup_opened');
+}
+
+editButton.addEventListener('click', togglePopupEditProfile);
+closeButton.addEventListener('click', togglePopupEditProfile);
+
+popupEditProfile.addEventListener('click', (event) => {
+    if (event.target === event.currentTarget) {
+        togglePopupEditProfile();
+    }
+})
+
+formEditProfile.addEventListener('submit', (event) => {
+    event.preventDefault();
+    profileName.textContent = inputsProfile[0].value;
+    description.textContent = inputsProfile[1].value;
+    togglePopupEditProfile();
+})
+
+// попап для добавления карточек. переименовать closeButton1
+let togglePopupAddCard = () => {
+  popupAddCard.classList.toggle('popup_opened');
+}
+
+addButton.addEventListener('click', togglePopupAddCard);
+closeButton1.addEventListener('click', togglePopupAddCard);
+
+popupAddCard.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) {
+      togglePopupAddCard();
+  }
+})
+
+// показываем 6 карточек на странице
 function render() {
   initialCards.forEach(renderItem);
 }
@@ -86,14 +86,20 @@ function render() {
 function renderItem(element) {
   const card = itemTemplate.cloneNode(true);
   card.querySelector('.element__image').src = element.link;
-  card.querySelector('.element__title').textContent = element.name;
+  card.querySelector('.element__image').alt = element.name;
+  card.querySelector('.element__title').innerText = element.name;
   list.appendChild(card);
 }
 
-/*function handleSubmit(evt) {
-  renderItem(formInput.value)
-}
-
-formButton.addEventListener('click', handleSubmit);*/
+// добавим карточку
+formAddCard.addEventListener('submit', (event) => {  
+  event.preventDefault();
+  const newElement = itemTemplate.cloneNode(true);
+  newElement.querySelector('.element__image').src = inputs[1].value;
+  newElement.querySelector('.element__image').alt = inputs[0].value;
+  newElement.querySelector('.element__title').innerText = inputs[0].value;
+  list.insertBefore(newElement, list.firstChild);
+  togglePopupAddCard()
+})
 
 render();
