ab180bd03b5577a5139daa44ec0f43c92ac23d4f
Sofia Puolakainen <sonja.puolaka@yandex.ru>
2021-02-28 23:37:10 +0300

    replaced config, new FormValidator,.enableValidation() to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -95,4 +95,20 @@ formAddCard.addEventListener('submit', (event) => {
   resetPopupAddCard();
 })
 
+
+const config = {
+  formSelector: '.popup',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_visible'
+}
+
+const formEditProfileValidator = new FormValidator(config, '.popup_type_edit');
+const formAddCardValidator = new FormValidator(config, '.popup_type_add');
+
+formEditProfileValidator.enableValidation();
+formAddCardValidator.enableValidation();
+
 export { openPopup }
