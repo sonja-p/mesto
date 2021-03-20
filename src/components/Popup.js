c844a1a0d3394958db0ecfedd8f7a842086124c5
Sofia Puolakainen <sonja.puolaka@yandex.ru>
2021-03-20 22:58:49 +0300

    changed the callback in window event listener

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,6 +1,7 @@
 export default class Popup {
     constructor(popupSelector) {
         this._popup = document.querySelector(popupSelector);
+        this._handleEscClose = this._handleEscClose.bind(this)
     }
 
     _handleEscClose(evt) {
@@ -18,12 +19,12 @@ export default class Popup {
     
     open() {  
         this._popup.classList.add('popup_opened');
-        window.addEventListener('keydown', this._handleEscClose.bind(this));
+        window.addEventListener('keydown', this._handleEscClose);
     }
 
     close() {
         this._popup.classList.remove('popup_opened');
-        window.removeEventListener('keydown', this._handleEscClose.bind(this));
+        window.removeEventListener('keydown', this._handleEscClose);
     }
 
     setEventListeners() {
